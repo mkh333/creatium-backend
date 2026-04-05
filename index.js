8d577f34b40cf57898c77f656f99015aa8d4050d
mkh333 <merytv2003@gmail.com>
2026-04-06 02:29:15 +0400

    update authApi and env

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 3100;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://creatium-backend.onrender.com"],
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
